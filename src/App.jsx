ce2638df84af0835d9b943c7176463c888191f0b
Le5ya <liliatolstyh@yandex.ru>
2023-02-22 22:02:10 +0300

    redux toolkit, Order&Catalog,count&key

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import './App.css'
-
+import { Provider } from 'react-redux'
+import { Catalog } from "./components/Catalog/Catalog.jsx"
 import { Header } from "./components/Header/Header.jsx"
 import { Navigation } from "./components/Navigation/Navigation.jsx"
-import { Catalog } from "./components/Catalog/Catalog.jsx"
+import { store } from './store/index.js'
 
 export const App = () => {
   
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <main>
         <Navigation />
         <Catalog />
       </main>
       <footer></footer>
-    </>
+    </Provider>
   )
 }
 
