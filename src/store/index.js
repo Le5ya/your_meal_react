a753325265efadd564915da8b5cfdf1fa9514db4
Le5ya <liliatolstyh@yandex.ru>
2023-02-28 18:13:03 +0300

    goods from the server

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoryReducer from './category/categorySlice.js';
+import productReducer from './product/productSlice.js';
 
 
 
 export const store = configureStore({
 	reducer: {
 		category: categoryReducer,
+		product: productReducer,
 	},
 });
---
+++ src/store/product/productSlice.js
@@ -0,0 +1,33 @@
+import { API_URI, POSTFIX } from '../../const.js';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+	products: [],
+	error: '',
+};
+export const productRequestAsync = createAsyncThunk(
+	'product/fetch', (category) =>
+	fetch(`${API_URI}${POSTFIX}?category=${category}`)
+		.then(req => req.json())
+		.catch(error => ({ error }))
+);
+const productSlice = createSlice({
+	name: 'product',
+	initialState,
+	extraReducers: builder => {
+		builder
+			.addCase(productRequestAsync.pending.type, state => {
+				state.error = ''
+			})
+			.addCase(productRequestAsync.fulfilled.type, (state, action) => {
+				state.error = '';
+				state.products = action.payload;
+			})
+			.addCase(productRequestAsync.rejected.type, (state, action) => {
+				state.error = action.payload.error
+			})
+	}
+});
+export default productSlice.reducer;
+
